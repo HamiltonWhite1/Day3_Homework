39ccf75d434cb28c9bbf9af1c28c57c42b445cf1
HamiltonWhite1 <92882456+HamiltonWhite1@users.noreply.github.com>
2022-02-03 07:48:39 -0500

    Commit with a new problem making use of the map function

--- listcomppractices.py
+++ listcomppractices.py
@@ -1,3 +1,9 @@
+# Find the needle in the haystack, and return the index
 def find_needle(haystack):
     return f"found the needle at position {[ind for ind, x in enumerate(haystack) if x == 'needle'].pop()}"
 print(find_needle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']))
+
+# Convert number to reversed array of digits
+def digitize(n):
+    return list(map(int, str(n)))[::-1]
+print(digitize(345))
